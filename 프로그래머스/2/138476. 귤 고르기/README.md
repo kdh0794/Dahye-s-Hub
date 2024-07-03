f1e13afe9aa93acbdaf2dba26d0ff21cd0143d34
kdh0794 <44249751+kdh0794@users.noreply.github.com>
2024-07-03 16:34:00 +0900

    [level 2] Title: 귤 고르기, Time: 31.42 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=csharp) 
 
 ### 성능 요약
 
-메모리: 37.2 MB, 시간: 31.04 ms
+메모리: 37 MB, 시간: 31.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 03일 15:21:41
+2024년 07월 03일 16:33:57
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
@@ -5,32 +5,32 @@ using System.Linq;
 public class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-var count = 0;
+        var count = 0;
 
-var tangerineGradeCount = new Dictionary<int, int>();
+        var tangerineGradeCount = new Dictionary<int, int>();
 
-foreach (var t in tangerine)
-{
-    if (tangerineGradeCount.ContainsKey(t))
-    {
-        tangerineGradeCount[t]++;
-    }
-    else
-    {
-        tangerineGradeCount[t] = 1;
-    }
-}
-
-var orderList = tangerineGradeCount.OrderByDescending(kvp => kvp.Value).ToList();
-foreach (var tancount in orderList)
-{
-    if (k <= count)
-        break;
-
-    count += tancount.Value;
-    answer++;
-}
-
-return answer;
+        foreach (var t in tangerine)
+        {
+                    if (tangerineGradeCount.ContainsKey(t))
+            {
+                tangerineGradeCount[t]++;
+            }
+            else
+            {
+                tangerineGradeCount[t] = 1;
+            }
+        }
+        
+        var orderList = tangerineGradeCount.OrderByDescending(kvp => kvp.Value).ToList();
+        foreach (var tancount in orderList)
+        {
+            if (k <= count)
+                break;
+        
+            count += tancount.Value;
+            answer++;
+        }
+        
+        return answer;
     }
 }
