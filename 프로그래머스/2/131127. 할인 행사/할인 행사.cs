ea5567e6c18b379e43791716f4c8a2d566664f17
kdh0794 <44249751+kdh0794@users.noreply.github.com>
2024-07-04 12:01:56 +0900

    [level 2] Title: 할인 행사, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Solution {
+    public int solution(string[] want, int[] number, string[] discount) {
+             int answer = 0;
+int firstDay = 0;
+
+for (int i = 0; i < want.Length;)
+{
+    int currentItemCount = number[i];
+    int maxDay = firstDay + 10;
+
+    if (maxDay > discount.Length)
+    {
+        break;
+    }
+
+    for (int j = firstDay; j < maxDay; j++)
+    {
+        if (want[i].Equals(discount[j]))
+        {
+            currentItemCount--;
+            if (currentItemCount <= 0)
+            {
+                i++;
+
+                if(i >= want.Length)
+                {
+                    firstDay++;
+                    answer++;
+                    i = 0;
+                }
+
+                break;
+            }
+        }
+    }
+
+    if (currentItemCount > 0)
+    {
+                    i = 0;
+        firstDay++;
+    }
+}
+
+return answer;
+    }
+}
